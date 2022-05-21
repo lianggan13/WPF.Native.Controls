25aa0f2c169a0240ee5b6ffdc174c9055390bc57
lianggan13 <676021630@qq.com>
2022-05-21 10:59:55 +0800

    s

---
+++ WPF.Common/Themes/CustomTheme.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WPF.Common.Themes
+{
+    public class CustomTheme
+    {
+        public static ComponentResourceKey DogBrushKey
+        {
+            get { return new ComponentResourceKey(typeof(CustomTheme), nameof(DogBrushKey)); }
+        }
+    }
+}
