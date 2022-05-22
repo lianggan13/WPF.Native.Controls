18d89c989bc97a421637164f74704a34cc7d7250
lianggan13 <676021630@qq.com>
2022-05-22 21:21:00 +0800

    s

---
+++ WPF.Graphics/Brushes/ImageBrushes.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WPF.Graphics.Brushes
+{
+    /// <summary>
+    /// ImageBrushes.xaml 的交互逻辑
+    /// </summary>
+    public partial class ImageBrushes : Page
+    {
+        public ImageBrushes()
+        {
+            InitializeComponent();
+            EllipseGeometry ellipse = new EllipseGeometry(new Point(50, 50), 50, 20);
+            RectangleGeometry rect = new RectangleGeometry(new Rect(50, 50, 50, 20), 5, 5);
+        }
+    }
+}
