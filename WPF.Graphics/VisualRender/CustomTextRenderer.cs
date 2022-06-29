91ccd35b868901da8a4954a55d52e1b8a98f0c57
lianggan13 <676021630@qq.com>
2022-06-30 07:46:49 +0800

    s

---
+++ WPF.Graphics/VisualRender/CustomTextRenderer.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Globalization;
+using System.Windows;
+
+
+namespace WPF.Graphics.VisualRender
+{
+    using System.Windows.Media;
+    public class CustomTextRenderer : FrameworkElement
+    {
+        FormattedText ftext = null;
+
+
+        public CustomTextRenderer()
+        {
+            var dpiInfo = VisualTreeHelper.GetDpi(Application.Current.MainWindow);
+            var d = dpiInfo.PixelsPerDip;
+
+            ftext = new FormattedText("Hello, world",
+               CultureInfo.CurrentUICulture, FlowDirection.LeftToRight,
+               new Typeface("Verdana"), 24, Brushes.Black, d);
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            double angle = 30;
+            double x = 0, y = 0;
+            RotateTransform form = new RotateTransform(angle, x, y);
+            //drawingContext.Pop();
+            drawingContext.PushTransform(form);
+            //drawingContext.Pop();
+
+            drawingContext.DrawRectangle(Brushes.Chocolate, null, new Rect(0, 0, 100, 50));
+
+            drawingContext.DrawText(ftext, new Point(0, 0));
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            ftext.MaxTextWidth = availableSize.Width;
+            ftext.MaxTextHeight = availableSize.Height;
+            return new Size(ftext.Width, ftext.Height);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            ftext.MaxTextWidth = finalSize.Width;
+            ftext.MaxTextHeight = finalSize.Height;
+            return finalSize;
+        }
+    }
+}
---
+++ WPF.TextBlock/FlowDocuments/ControlViewer.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace WPF.TextBlock.FlowDocuments
+{
+    /// <summary>
+    /// ControlViewer.xaml 的交互逻辑
+    /// </summary>
+    public partial class ControlViewer : Page
+    {
+        public ControlViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            scViewer.Document = (FlowDocument)Application.LoadComponent(
+                new Uri("./FlowDocuments/MyFlowDocument.xaml", UriKind.Relative));
+
+            pgViewer.Document = (FlowDocument)Application.LoadComponent(
+               new Uri("./FlowDocuments/MyFlowDocument.xaml", UriKind.Relative));
+
+            reader.Document = (FlowDocument)Application.LoadComponent(
+               new Uri("./FlowDocuments/MyFlowDocument.xaml", UriKind.Relative));
+        }
+    }
+}
