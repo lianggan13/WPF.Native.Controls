e7242859ac4203e5ee49803c024eed1ebd9e9435
zhangliang <676021630@qq.com>
2022-07-18 10:23:05 +0800

    s

---
+++ WPF.Animation/Clock/ClockPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿namespace WPF.Animation.Clock
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using System.Windows.Media.Animation;
+    using System.Windows.Shapes;
+
+
+    /// <summary>
+    /// ClockPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class ClockPage : Page
+    {
+        public ClockPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                // Example 16-33. Selecting a storyboard at runtime
+
+                bool isMorning = DateTime.Now.Hour < 12;
+                string storyboardKey = isMorning ? "sunrise" : "sunset";
+                Storyboard sb = (Storyboard)FindResource(storyboardKey);
+
+                sb.Begin(myEllipse);
+
+                // End of Example 16-33.
+            }
+            else
+            {
+                if (clock == null)
+                {
+                    StartAnimation();
+                }
+                else if (paused)
+                {
+                    ResumeAnimation();
+                    paused = false;
+                }
+                else
+                {
+                    PauseAnimation();
+                    paused = true;
+                }
+            }
+        }
+        bool paused = false;
+
+
+        // Example 16-34. Controlling animations with a clock
+
+        AnimationClock clock;
+
+        void StartAnimation()
+        {
+            DoubleAnimation animate = new DoubleAnimation();
+            animate.To = 300;
+            animate.Duration = new Duration(TimeSpan.FromSeconds(5));
+            animate.RepeatBehavior = RepeatBehavior.Forever;
+
+            clock = animate.CreateClock();
+            myEllipse.ApplyAnimationClock(Ellipse.WidthProperty, clock);
+        }
+
+        void PauseAnimation()
+        {
+            clock.Controller.Pause();
+        }
+
+        void ResumeAnimation()
+        {
+            clock.Controller.Resume();
+        }
+    }
+}
---
+++ WPF.Navigate/TransAnimation/Window1.xaml.cs
@@ -0,0 +1,75 @@
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+
+
+namespace AnimateNavigateTransitions
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+
+    public partial class Window1 : System.Windows.Window
+    {
+
+        public Window1()
+        {
+            InitializeComponent();
+
+            mainFrame.Navigating += new System.Windows.Navigation.NavigatingCancelEventHandler(mainFrame_Navigating);
+            mainFrame.Navigated += new NavigatedEventHandler(mainFrame_Navigated);
+        }
+
+
+        // Example 16-36. Copying the outgoing page to a VisualBrush
+
+        VisualBrush lastPageBrush;
+        void mainFrame_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            Page lastPage = mainFrame.Content as Page;
+            if (lastPage != null)
+            {
+                lastPageBrush = new VisualBrush(lastPage);
+                lastPageBrush.Viewbox = new Rect(0, 0, lastPage.ActualWidth,
+                                                       lastPage.ActualHeight);
+                lastPageBrush.ViewboxUnits = BrushMappingMode.Absolute;
+                lastPageBrush.Stretch = Stretch.None;
+
+
+                // Page won't be at origin, thanks to navigation bar.
+                // Discover the offset.
+                Point pageOffset =
+                    lastPage.TransformToVisual(this).Transform(new Point());
+                transitionPlaceholder.Margin = new Thickness(pageOffset.X, pageOffset.Y,
+                                                             0, 0);
+
+            }
+            else
+            {
+                lastPageBrush = null;
+            }
+        }
+
+        // End of Example 16-36.
+
+
+        // Example 16-37. Running the transition animation
+
+        void mainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            if (lastPageBrush != null)
+            {
+                transitionPlaceholder.Background = lastPageBrush;
+                lastPageBrush = null;
+
+                Storyboard sb = (Storyboard)FindResource("transitionAnimation");
+                sb.Begin(this);
+            }
+        }
+
+        // End of Example 16-37.
+
+    }
+}
